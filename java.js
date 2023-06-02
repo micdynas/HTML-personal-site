4d9ae793a2aec40f9411ca12ac1bdb9e5551f6fa
micdynas <117646282+micdynas@users.noreply.github.com>
2023-06-02 10:51:55 +0100

    Add files via upload

--- java.js
+++ java.js
@@ -1,5 +1,5 @@
-let nav = document.querySelector("ul")
+let ul = document.querySelector("ul")
 let btn = document.querySelector(".menu_btn")
 
 
-btn.addEventListener("click", () => ul.classList.toggle("ul"))
+btn.addEventListener("onclick", () => ul.classList.toggle("ul"))
---
+++ new.js
@@ -0,0 +1,24 @@
+let div = document.querySelector("div")
+div.addEventListener( "click", random) 
+function random() {
+
+    let h = Math.random()*300
+let w = Math.random()*300
+div.style.width=` ${w}`
+div.style.height=` ${h}`
+
+let r =  Math.random()*255
+let g =  Math.random()*255
+let b =  Math.random()*255
+div.style.backgroundColor = `rgb(${r},${g},${b})`
+
+}
+
+
+window.addEventListener("mousemove", (e)=>{
+
+        div.style.left = e.clientX + "px"
+        div.style.top = e.clientY + "px"
+
+
+})
